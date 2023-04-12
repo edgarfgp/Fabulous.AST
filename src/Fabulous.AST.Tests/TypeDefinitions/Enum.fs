03731cf74884769fc477a3b9857335bcd00110d5
edgardev <edgar.gonzalez@fundourselves.com>
2023-04-12 16:55:00 +0100

    Enum support

---
+++ src/Fabulous.AST.Tests/TypeDefinitions/Enum.fs
@@ -0,0 +1,134 @@
+namespace Fabulous.AST.Tests.TypeDefinitions
+
+open Fabulous.AST.Tests
+open FSharp.Compiler.Text
+open Fantomas.Core.SyntaxOak
+open NUnit.Framework
+
+open Fabulous.AST
+
+open type Fabulous.AST.Ast
+
+module Enum =
+
+    [<Test>]
+    let ``Produces an enum`` () =
+        AnonymousModule() {
+            Enum("Colors") {
+                EnumCase("Red", "0")
+                EnumCase("Green", "1")
+                EnumCase("Blue", "2")
+            }
+        }
+
+        |> produces
+            """
+
+type Colors =
+    | Red = 0
+    | Green = 1
+    | Blue = 2
+"""
+
+    [<Test>]
+    let ``Produces an enum with SingleTextNode`` () =
+        AnonymousModule() {
+            Enum(SingleTextNode("Colors", Range.Zero)) {
+                EnumCase("Red", "0")
+                EnumCase("Green", "1")
+                EnumCase("Blue", "2")
+            }
+        }
+        |> produces
+            """
+
+type Colors =
+    | Red = 0
+    | Green = 1
+    | Blue = 2
+
+"""
+
+    [<Test>]
+    let ``Produces an enum case using EnumCaseNode`` () =
+        AnonymousModule() {
+            Enum("Colors") {
+                EnumCase("Red", "0")
+                EnumCase("Green", "1")
+                EnumCase("Blue", "2")
+
+                EnumCaseNode(
+                    None,
+                    None,
+                    None,
+                    SingleTextNode("Black", Range.Zero),
+                    SingleTextNode("=", Range.Zero),
+                    Expr.Constant(Constant.FromText(SingleTextNode("3", Range.Zero))),
+                    Range.Zero
+                )
+            }
+        }
+        |> produces
+            """
+
+type Colors =
+    | Red = 0
+    | Green = 1
+    | Blue = 2
+    | Black = 3
+
+"""
+
+    [<Test>]
+    let ``Produces an enum case using Widget and escape hatch`` () =
+        let enumCaseNode =
+            EnumCaseNode(
+                None,
+                None,
+                None,
+                SingleTextNode("Black", Range.Zero),
+                SingleTextNode("=", Range.Zero),
+                Expr.Constant(Constant.FromText(SingleTextNode("3", Range.Zero))),
+                Range.Zero
+            )
+
+        AnonymousModule() {
+            Enum("Colors") {
+                EnumCase("Red", "0")
+                EnumCase("Green", "1")
+                EnumCase("Blue", "2")
+                EscapeHatch(enumCaseNode)
+            }
+        }
+        |> produces
+            """
+
+type Colors =
+    | Red = 0
+    | Green = 1
+    | Blue = 2
+    | Black = 3
+
+"""
+
+    [<Test>]
+    let ``Produces an enum case for a list`` () =
+        let colors = [ "Red"; "Green"; "Blue"; "Black" ]
+
+        AnonymousModule() {
+            Enum("Colors") {
+                for i = 0 to colors.Length - 1 do
+                    EnumCase(colors.[i], $"{i}")
+            }
+
+        }
+        |> produces
+            """
+
+type Colors =
+    | Red = 0
+    | Green = 1
+    | Blue = 2
+    | Black = 3
+
+"""
