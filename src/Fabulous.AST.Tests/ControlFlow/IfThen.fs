892cbb09c7089021be2e79cc33195244ff772474
edgardev <edgar.gonzalez@fundourselves.com>
2023-04-21 09:42:53 +0100

    remove duplicated test

--- src/Fabulous.AST.Tests/ControlFlow/IfThen.fs
+++ src/Fabulous.AST.Tests/ControlFlow/IfThen.fs
@@ -9,34 +9,6 @@ open Fantomas.Core.SyntaxOak
 open NUnit.Framework
 
 module IfThen =
-
-    [<Test>]
-    let ``Produces if-then expression`` () =
-        let ifExp =
-            Expr.InfixApp(
-                ExprInfixAppNode(
-                    Expr.Ident(SingleTextNode("x", Range.Zero)),
-                    SingleTextNode("=", Range.Zero),
-                    Expr.Ident(SingleTextNode("12", Range.Zero)),
-                    Range.Zero
-                )
-            )
-
-        let thenExpr =
-            Expr.Constant(
-                Constant.Unit(UnitNode(SingleTextNode("(", Range.Zero), SingleTextNode(")", Range.Zero), Range.Zero))
-            )
-
-        AnonymousModule() {
-            IfThen(EscapeHatch(ifExp)) { EscapeHatch(ComputationExpressionStatement.OtherStatement(thenExpr)) }
-        }
-        |> produces
-            """
-
-if x = 12 then
-    ()
-"""
-
     [<Test>]
     let ``Produces if-then expression with EscapeHatch`` () =
         let ifExp =
