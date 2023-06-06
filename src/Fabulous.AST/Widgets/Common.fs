946334b99c3a1c0098e81498e75c04af5815cb21
edgardev <edgar.gonzalez@fundourselves.com>
2023-06-06 21:23:57 +0100

    fix rebase issues

--- src/Fabulous.AST/Widgets/Common.fs
+++ src/Fabulous.AST/Widgets/Common.fs
@@ -10,6 +10,97 @@ type AccessControl =
 
 [<AutoOpen>]
 module Auxiliary =
+    type BindingNode with
+
+        static member Create(name: string, value: string, ?t: Type) =
+            match t with
+            | None ->
+                BindingNode(
+                    None,
+                    None,
+                    MultipleTextsNode([ SingleTextNode("let", Range.Zero) ], Range.Zero),
+                    false,
+                    None,
+                    None,
+                    Choice1Of2(IdentListNode([ IdentifierOrDot.Ident(SingleTextNode(name, Range.Zero)) ], Range.Zero)),
+                    None,
+                    List.Empty,
+                    None,
+                    SingleTextNode("=", Range.Zero),
+                    Expr.Constant(Constant.FromText(SingleTextNode(value, Range.Zero))),
+                    Range.Zero
+                )
+            | Some t ->
+                BindingNode(
+                    None,
+                    None,
+                    MultipleTextsNode([ SingleTextNode("let", Range.Zero) ], Range.Zero),
+                    false,
+                    None,
+                    None,
+                    Choice1Of2(IdentListNode([ IdentifierOrDot.Ident(SingleTextNode(name, Range.Zero)) ], Range.Zero)),
+                    None,
+                    List.Empty,
+                    Some(BindingReturnInfoNode(SingleTextNode(":", Range.Zero), t, Range.Zero)),
+                    SingleTextNode("=", Range.Zero),
+                    Expr.Constant(Constant.FromText(SingleTextNode(value, Range.Zero))),
+                    Range.Zero
+                )
+
+    type MemberDefnAbstractSlotNode with
+
+        static member Method(identifier: string, parameters: (Type * SingleTextNode) list, returnType: Type) =
+            MemberDefnAbstractSlotNode(
+                None,
+                None,
+                MultipleTextsNode(
+                    [ SingleTextNode("abstract", Range.Zero); SingleTextNode("member", Range.Zero) ],
+                    Range.Zero
+                ),
+                SingleTextNode(identifier, Range.Zero),
+                None,
+                Type.Funs(TypeFunsNode(parameters, returnType, Range.Zero)),
+                None,
+                Range.Zero
+            )
+
+        static member Property(identifier: string, returnType: Type) =
+            MemberDefnAbstractSlotNode(
+                None,
+                None,
+                MultipleTextsNode(
+                    [ SingleTextNode("abstract", Range.Zero); SingleTextNode("member", Range.Zero) ],
+                    Range.Zero
+                ),
+                SingleTextNode(identifier, Range.Zero),
+                None,
+                Type.Funs(TypeFunsNode([], returnType, Range.Zero)),
+                None,
+                Range.Zero
+            )
+
+        static member GetSet(identifier: string, returnType: Type) =
+            MemberDefnAbstractSlotNode(
+                None,
+                None,
+                MultipleTextsNode(
+                    [ SingleTextNode("abstract", Range.Zero); SingleTextNode("member", Range.Zero) ],
+                    Range.Zero
+                ),
+                SingleTextNode(identifier, Range.Zero),
+                None,
+                Type.Funs(TypeFunsNode([], returnType, Range.Zero)),
+                Some(
+                    MultipleTextsNode(
+                        [ SingleTextNode("with", Range.Zero)
+                          SingleTextNode("get,", Range.Zero)
+                          SingleTextNode("set", Range.Zero) ],
+                        Range.Zero
+                    )
+                ),
+                Range.Zero
+            )
+
     type Type with
 
         /// <summary>Create a type from a string.</summary>
@@ -24,7 +115,6 @@ module Auxiliary =
             // Bad example would be: "int -> int", use Type.Fun instead.
             IdentListNode([ IdentifierOrDot.Ident(SingleTextNode(typeName, Range.Zero)) ], Range.Zero)
             |> Type.LongIdent
-
 
 module internal TypeHelpers =
     let inline createAttributes values =
