945d923dec4133e9a48ba3825ff3e7cb3ef77668
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2024-01-13 13:45:43 +0000

    Initial support for unit of measures

---
+++ src/Fabulous.AST.Tests/TypeDefinitions/UnitsOfMeasure.fs
@@ -0,0 +1,23 @@
+namespace Fabulous.AST.Tests.TypeDefinitions
+
+open Fabulous.AST.Tests
+open NUnit.Framework
+
+open Fabulous.AST
+
+open type Fabulous.AST.Ast
+
+module UnitsOfMeasure =
+
+    [<Test>]
+    let ``Produces type Unit of measure`` () =
+        AnonymousModule() {
+            UnitsOfMeasure("cm")
+        }
+
+        |> produces
+            """
+
+[<Measure>]
+type cm
+"""
---
+++ src/Fabulous.AST/Widgets/TypeDefinitions/UnitsOfMeasure.fs
@@ -0,0 +1,68 @@
+namespace Fabulous.AST
+
+open System.Runtime.CompilerServices
+open Fantomas.FCS.Text
+open Fabulous.AST.StackAllocatedCollections
+open Fantomas.Core.SyntaxOak
+open Fabulous.AST.StackAllocatedCollections.StackList
+
+module UnitsOfMeasure =
+
+    let Name = Attributes.defineWidget "Name"
+
+    let WidgetKey =
+        Widgets.register "UnitsOfMeasure" (fun widget ->
+            let name = Helpers.getNodeFromWidget<SingleTextNode> widget Name
+            TypeNameNode(
+                None,
+                Some(MultipleAttributeListNode.Create([ "Measure" ])),
+                SingleTextNode.``type``,
+                Some(name),
+                IdentListNode([], Range.Zero),
+                None,
+                [],
+                None,
+                None,
+                None,
+                Range.Zero
+            ))
+
+[<AutoOpen>]
+module UnitsOfMeasureBuilders =
+    type Ast with
+
+        static member inline UnitsOfMeasure(name: WidgetBuilder<#SingleTextNode>) =
+            WidgetBuilder<TypeNameNode>(
+                UnitsOfMeasure.WidgetKey,
+                AttributesBundle(
+                    StackList.empty(),
+                    ValueSome [| UnitsOfMeasure.Name.WithValue(name.Compile()) |],
+                    ValueNone
+                )
+            )
+
+        static member inline UnitsOfMeasure(name: SingleTextNode) =
+            Ast.UnitsOfMeasure(Ast.EscapeHatch(name))
+
+        static member inline UnitsOfMeasure(name: string) =
+            Ast.UnitsOfMeasure(SingleTextNode(name, Range.Zero))
+
+[<Extension>]
+type MeasureYieldExtensions =
+    [<Extension>]
+    static member inline Yield
+        (
+            _: CollectionBuilder<'parent, ModuleDecl>,
+            x: WidgetBuilder<TypeNameNode>
+        ) : CollectionContent =
+        let node = Tree.compile x
+        let typeDefn = TypeDefn.None(node)
+        let typeDefn = ModuleDecl.TypeDefn(typeDefn)
+        let widget = Ast.EscapeHatch(typeDefn).Compile()
+        { Widgets = MutStackArray1.One(widget) }
+
+    [<Extension>]
+    static member inline Yield(_: CollectionBuilder<'parent, ModuleDecl>, x: TypeNameNode) : CollectionContent =
+        let moduleDecl = ModuleDecl.TypeDefn(TypeDefn.None(x))
+        let widget = Ast.EscapeHatch(moduleDecl).Compile()
+        { Widgets = MutStackArray1.One(widget) }
