17f28ce2a3f459e5cfe63d277537a6930a881362
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2024-03-10 15:11:31 +0000

    Escape content in from String

--- src/Fabulous.AST.Tests/TypeDefinitions/Record.fs
+++ src/Fabulous.AST.Tests/TypeDefinitions/Record.fs
@@ -14,13 +14,13 @@ module Record =
     [<InlineData(" First Name ", "``First Name``")>]
     [<InlineData("First_Name", "First_Name")>]
     [<InlineData("net.6", "``net.6``")>]
-    [<InlineData("net.6", "``net.6``")>]
+    [<InlineData(" net.6 ", "``net.6``")>]
     let ``Produces a record with fields with backticks`` (value: string) (expected: string) =
         AnonymousModule() { Record("Person ") { Field(value, LongIdent("int")) } }
         |> produces
             $$"""
 
-type Person = { {{expected}: int }
+type Person = { {{expected}}: int }
 
 """
 
