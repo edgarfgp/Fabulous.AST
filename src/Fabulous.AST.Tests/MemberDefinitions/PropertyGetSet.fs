88e2c289e2e05c31c36c96a71ea03c7d0226a2f0
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2024-04-13 18:54:19 +0100

    more tests

--- src/Fabulous.AST.Tests/MemberDefinitions/PropertyGetSet.fs
+++ src/Fabulous.AST.Tests/MemberDefinitions/PropertyGetSet.fs
@@ -21,6 +21,33 @@ module PropertyGetSet =
                         GetterBinding(Unquoted("name")),
                         SetterBinding(NamedPat("value"), Unquoted("()"))
                     )
+
+                    Property(
+                        "this.Age",
+                        GetterBinding(Unquoted("age")),
+                        SetterBinding(NamedPat("value"), ConstantExpr(ConstantUnit()))
+                    )
+                        .toInlined()
+
+                    Property(
+                        "this.Name2",
+                        GetterBinding(Unquoted("name")).toInlined(),
+                        SetterBinding(NamedPat("value"), Unquoted("()"))
+                    )
+                        .toPrivate()
+
+                    Property(
+                        "this.FirstName",
+                        GetterBinding(Unquoted("firstName")),
+                        SetterBinding(ParenPat(NamedPat("value")), Unquoted("()"))
+                    )
+
+                    Property(
+                        "this.LastName",
+                        GetterBinding(Unquoted("lastName")).returnType(String()),
+                        SetterBinding(ParenPat(ParameterPat("value", String())), ConstantExpr(ConstantUnit()))
+                            .returnType(Unit())
+                    )
                 }
 
             }
@@ -31,5 +58,21 @@ type Person (name: string, age: int) =
     member this.Name
         with get () = name
         and set value = ()
+
+    member inline this.Age
+        with get () = age
+        and set value = ()
+
+    member private this.Name2
+        with inline get () = name
+        and set value = ()
+
+    member this.FirstName
+        with get () = firstName
+        and set (value) = ()
+
+    member this.LastName
+        with get (): string = lastName
+        and set (value: string): unit = ()
 
 """
