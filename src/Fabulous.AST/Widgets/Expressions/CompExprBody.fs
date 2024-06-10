3666fef56f27bbf7f51d8d634382ac8ae362e31b
Edgar Gonzalez <edgar.gonzalez@fundourselves.com>
2024-06-11 00:15:12 +0200

    improve constructors

--- src/Fabulous.AST/Widgets/Expressions/CompExprBody.fs
+++ src/Fabulous.AST/Widgets/Expressions/CompExprBody.fs
@@ -18,22 +18,31 @@ module CompExprBody =
 module CompExprBodyBuilders =
     type Ast with
 
-        static member CompExprBodyExpr(value: WidgetBuilder<ComputationExpressionStatement> list) =
-            let statements = value |> List.map Gen.mkOak
+        static member CompExprBodyExpr(values: WidgetBuilder<ComputationExpressionStatement> list) =
+            let statements = values |> List.map Gen.mkOak
 
             WidgetBuilder<Expr>(
                 CompExprBody.WidgetKey,
                 AttributesBundle(StackList.one(CompExprBody.Statements.WithValue(statements)), Array.empty, Array.empty)
             )
 
+        static member CompExprBodyExpr(value: WidgetBuilder<ComputationExpressionStatement>) =
+            Ast.CompExprBodyExpr([ value ])
+
         static member CompExprBodyExpr(values: WidgetBuilder<Expr> list) =
             let values = values |> List.map(fun e -> Ast.OtherExpr(e))
             Ast.CompExprBodyExpr(values)
+
+        static member CompExprBodyExpr(value: WidgetBuilder<Expr>) = Ast.CompExprBodyExpr([ value ])
 
         static member CompExprBodyExpr(values: string list) =
             let values = values |> List.map(fun e -> Ast.OtherExpr(e))
             Ast.CompExprBodyExpr(values)
 
+        static member CompExprBodyExpr(value: string) = Ast.CompExprBodyExpr([ value ])
+
         static member CompExprBodyExpr(values: WidgetBuilder<BindingNode> list) =
             let values = values |> List.map(fun e -> Ast.LetOrUseExpr(e))
             Ast.CompExprBodyExpr(values)
+
+        static member CompExprBodyExpr(value: WidgetBuilder<BindingNode>) = Ast.CompExprBodyExpr([ value ])
