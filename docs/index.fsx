6989c420db9cbd2d2f4e739e56cb211c8f540434
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-12-14 23:06:23 +0000

    Update readme

--- docs/index.fsx
+++ docs/index.fsx
@@ -7,13 +7,146 @@ index: 0
 
 # Fabulous.AST
 
-Welcome to the Fabulous.AST, an Abstract Syntax Tree (AST) Domain Specific Language (DSL) for F#.
+#### What is an AST?
 
-Fabulous.AST uses [Fantomas](https://fsprojects.github.io/fantomas/docs/end-users/GeneratingCode.html) to generate F# code from AST. This means that you can use Fabulous.AST to generate F# code that is formatted according to the Fantomas style guide. It's designed to provide a simple and expressive way to represent code as a tree of nodes. This makes it easier to manipulate and analyze code programmatically.
+`AST` stands for Abstract Syntax Tree. It is a tree representation of the abstract syntactic structure of source code written in a programming language.
+It is used by compilers to analyze, transform, and generate code.
 
-## Plain Fantomas
+You can generate code by just using strings and string interpolation. But there are several reasons why you should not do that:
 
-Let's take a look at an AST example in Fantomas:
+- It's error-prone and hard to maintain.
+- If the code you are generating is complex, then it's even harder to generate it using string interpolation.
+- You will have to write some extra code to handle edge cases and make sure that the generated code is valid, handle formatting and indentation.
+
+This is a simple example of generating code using `StringBuilder` and string interpolation:
+*)
+
+open System.Text
+let code = StringBuilder()
+code.AppendLine("module MyModule =")
+code.AppendLine("    let x = 12")
+code |> string |> printfn "%s"
+
+// produces the following code:
+(*** include-output ***)
+
+(**
+#### Why use AST to generate code?
+
+ASTs are more verbose than string interpolation and requires you to think in terms of nodes and trees, which can be a bit hard to grasp at first, but it becomes very powerful when you use it to generate code.
+
+Some of the benefits of using AST to generate code are:
+
+- The code generated is compliant with the F# syntax and is will most likely be valid and compilable.
+- It provides a more structured way to generate code.
+- It allows you to manipulate the code programmatically.
+- It allows you to generate more complex code in a more maintainable way.
+
+#### How to use AST to generate code?
+
+Let's explore three different ways to generate code using AST:
+
+##### 1. Compiler AST
+
+- It is a very verbose and hard to read.
+- It is Hard manipulate or analyze programmatically.
+- Contains a lot of information that is not relevant to the code itself.
+
+You can see a live example using [Fantomas tools](https://fsprojects.github.io/fantomas-tools/#/ast?data=N4KABGBEAmCmBmBLAdrAzpAXFSAacUiaAYmolmPAIYA2as%2BEkAxgPZwWQ2wAuYAHmAC8YAIwAmPAUix%2BAByrJoFHgCcArrBABfIA)
+*)
+
+open Fantomas.FCS.Syntax
+open Fantomas.FCS.SyntaxTrivia
+open Fantomas.FCS.Text
+open Fantomas.FCS.Xml
+
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.FCS.dll"
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.Core.dll"
+
+ParsedInput.ImplFile(
+    ParsedImplFileInput.ParsedImplFileInput(
+        fileName = "tmp.fsx",
+        isScript = true,
+        qualifiedNameOfFile = QualifiedNameOfFile.QualifiedNameOfFile(Ident("Tmp$fsx", Range.Zero)),
+        scopedPragmas = [],
+        hashDirectives = [],
+        contents =
+            [ SynModuleOrNamespace.SynModuleOrNamespace(
+                  longId = [ Ident("Tmp", Range.Zero) ],
+                  isRecursive = false,
+                  kind = SynModuleOrNamespaceKind.AnonModule,
+                  decls =
+                      [ SynModuleDecl.Let(
+                            isRecursive = false,
+                            bindings =
+                                [ SynBinding.SynBinding(
+                                      accessibility = None,
+                                      kind = SynBindingKind.Normal,
+                                      isInline = false,
+                                      isMutable = false,
+                                      attributes = [],
+                                      xmlDoc = PreXmlDoc.Empty,
+                                      valData =
+                                          SynValData.SynValData(
+                                              memberFlags = None,
+                                              valInfo =
+                                                  SynValInfo.SynValInfo(
+                                                      curriedArgInfos = [],
+                                                      returnInfo =
+                                                          SynArgInfo.SynArgInfo(
+                                                              attributes = [],
+                                                              optional = false,
+                                                              ident = None
+                                                          )
+                                                  ),
+                                              thisIdOpt = None
+                                          ),
+                                      headPat =
+                                          SynPat.Named(
+                                              ident = SynIdent.SynIdent(ident = Ident("x", Range.Zero), trivia = None),
+                                              isThisVal = false,
+                                              accessibility = None,
+                                              range = Range.Zero
+                                          ),
+                                      returnInfo = None,
+                                      expr = SynExpr.Const(constant = SynConst.Int32(12), range = Range.Zero),
+                                      range = Range.Zero,
+                                      debugPoint = DebugPointAtBinding.Yes(Range.Zero),
+                                      trivia =
+                                          { LeadingKeyword = SynLeadingKeyword.Let(Range.Zero)
+                                            InlineKeyword = None
+                                            EqualsRange = Some(Range.Zero) }
+                                  ) ],
+                            range = Range.Zero
+                        ) ],
+                  xmlDoc = PreXmlDoc.Empty,
+                  attribs = [],
+                  accessibility = None,
+                  range = Range.Zero,
+                  trivia = { LeadingKeyword = SynModuleOrNamespaceLeadingKeyword.None }
+              ) ],
+        flags = (false, false),
+        trivia =
+            { ConditionalDirectives = []
+              CodeComments = [] },
+        identifiers = set []
+    )
+)
+
+// produces the following code:
+let x = 12
+
+(**
+##### 2. Fantomas Oak AST
+
+It is a simplified version of the official AST that is used by Fantomas to format F# code.
+
+- It is more concise and easier to read.
+- It is a bit easier to manipulate or analyze programmatically.
+- It is a bit more human-readable. as it contains only the relevant information about the code itself.
+- But it's still not very easy to work with, as we will need to provide a lot of optional values even for simple code.
+
+You can see a live example using [Fantomas tools](https://fsprojects.github.io/fantomas-tools/#/oak?data=N4KABGBEDGD2AmBTSAuKAbRAXMAPMAvGAIwBMkANOFEgGYCWAdogM6pSXWT0sBiL9drQCG6FoioRuLAOIAnYQAcAFgDV6iAO5DR4kAF8gA)
 *)
 
 #r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.FCS.dll"
@@ -53,9 +186,33 @@ Oak(
 
 // produces the following code:
 (*** include-output ***)
+
 (**
 
-## Using Fabulous.AST
+##### 3. Fabulous.AST DSL
+
+So far we have seen how to generate code using the official F# compiler AST and Fantomas Oak AST.
+You might be thinking that it's not worth the effort to use ASTs to generate code and still prefer string interpolation. But that's where `Fabulous.AST` comes in.
+
+`Fabulous.AST` library provides a more user-friendly API to generate code using ASTs.
+It is built on top of Fantomas Oak AST and provides a more concise and easier to use API to generate code.
+It aims to really cut down on the boilerplate code required to generate code.
+
+You can even provide your own configuration to format the code as you like by using `FormatConfig` record type.
+
+```fsharp
+type FormatConfig =
+    { IndentSize: Num
+      MaxLineLength: Num
+      EndOfLine: EndOfLineStyle
+      InsertFinalNewline: bool
+      SpaceBeforeParameter: bool
+      SpaceBeforeLowercaseInvocation: bool
+      SpaceBeforeUppercaseInvocation: bool
+      SpaceBeforeClassConstructor: bool
+      SpaceBeforeMember: bool
+      SpaceBeforeColon: bool }
+```
 
 Now let's take a look at same example using Fabulous.AST:
 *)
@@ -66,7 +223,7 @@ Now let's take a look at same example using Fabulous.AST:
 open Fabulous.AST
 open type Fabulous.AST.Ast
 
-Oak() { AnonymousModule() { Value("y", "12") } }
+Oak() { AnonymousModule() { Value("x", "12") } }
 |> Gen.mkOak
 |> Gen.run
 |> printfn "%s"
@@ -74,31 +231,167 @@ Oak() { AnonymousModule() { Value("y", "12") } }
 (*** include-output ***)
 
 (**
-### Escape Hatch
+#### What widgets to use to generate the code?
+Fabulous.AST maps the Fantomas Oak AST nodes. So you can use [Fantomas tools](https://fsprojects.github.io/fantomas-tools/#/oak?data=N4KABGBEDGD2AmBTSAuKAbRAXMAPMAvGAIwBMAOgHaQA04USAZgJaWIDOqUt9kz7AMXbMujAIbp2iOhD7sA4gCcxABwAWANWaIA7qIlSQAXyA) to take a peak at the AST nodes and then use the corresponding widgets to generate the code.
 
-You can use an `Escape Hatch` to generate code that is not supported by Fabulous.AST yet by constructing the raw [Fantomas.SyntaxOak](https://fsprojects.github.io/fantomas/reference/fantomas-core-syntaxoak.html) node.
-For example, the following code: *)
+For example, the following Oak AST node:
+```fsharp
+Oak (1,0-1,10)
+    ModuleOrNamespaceNode (1,0-1,10)
+        BindingNode (1,0-1,10)
+            MultipleTextsNode (1,0-1,3)
+                let (1,0-1,3)
+            IdentListNode (1,4-1,5)
+                x (1,4-1,5)
+            = (1,6-1,7)
+            12 (1,8-1,10)
+```
+
+Translates to the following Fabulous.AST code:
+
+```fsharp
+Oak() { AnonymousModule() { Value("x", "12") } }
+```
+
+We have cut down the boilerplate code from 70 lines to just 5 lines of code. And it's much easier to read and understand.
+But we can generate much more complex code using Fabulous.AST. Let's take a look at some more examples.
+
+#### Example 1: Generate record type
+- We will generate a record type with two fields `Name` and `Age`.
+- We will use `Namespace`, `Module` and `Record` widgets to generate the code.
+- We want the record to be formatted using `Stroustrup` style.
+*)
+
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fabulous.AST.dll"
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.FCS.dll"
+
+open Fabulous.AST
+open type Fabulous.AST.Ast
+
+let formatConfig =
+    { FormatConfig.Default with
+        MultilineBracketStyle = Stroustrup
+        RecordMultilineFormatter = NumberOfItems }
+
+Oak() {
+    Namespace("MyNamespace") {
+        Module("Records") {
+            (Record("Person") {
+                Field("Name", "string")
+                Field("Age", "int")
+            })
+                .members() {
+                Member("this.Name", InterpolatedStringExpr([ Constant("this.Name"); Constant("this.Age") ]))
+            }
+        }
+    }
+}
+|> Gen.mkOak
+|> Gen.runWith formatConfig
+|> printfn "%s"
+// produces the following code:
+(*** include-output ***)
+
+(**
+#### Example 2: Generate a discriminated union type
+- We will generate a discriminated union type with two cases `Some 'a` and `None`.
+- We will use AnonymousModule, Union and UnionCase widgets to generate the code.
+*)
+
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fabulous.AST.dll"
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.FCS.dll"
 
 open type Fabulous.AST.Ast
 
-let topLevelBinding =
-    BindingNode(
-        xmlDoc = None,
-        attributes = None,
-        leadingKeyword = MultipleTextsNode([ SingleTextNode("let", Range.Zero) ], Range.Zero),
-        isMutable = false,
-        inlineNode = None,
-        accessibility = Some(SingleTextNode("private", Range.Zero)),
-        functionName = Choice1Of2(IdentListNode([ IdentifierOrDot.Ident(SingleTextNode("x", Range.Zero)) ], Range.Zero)),
-        genericTypeParameters = None,
-        parameters = List.Empty,
-        returnType = None,
-        equals = SingleTextNode("=", Range.Zero),
-        expr = Expr.Constant(Constant.FromText(SingleTextNode("12", Range.Zero))),
-        range = Range.Zero
-    )
+Oak() {
+    AnonymousModule() {
+        Union("Option") {
+            UnionCase("Some", "'a")
+                .triviaBefore(SingleLine("Represents the Some case with a value."))
 
-Oak() { AnonymousModule() { EscapeHatch(topLevelBinding) } }
+            UnionCase("None").triviaBefore(SingleLine("Represents the None case."))
+        }
+        |> _.typeParams(PostfixList("'a"))
+        |> _.xmlDocs([ "Represents the option type." ])
+    }
+}
+|> Gen.mkOak
+|> Gen.run
+|> printfn "%s"
+// produces the following code:
+(*** include-output ***)
+
+(**
+#### Example 3: Generate a class type with explicit constructor
+- We will generate a class type with explicit constructor with two fields `Name`, LastName` and `Age`.
+- We will use AnonymousModule, TypeDefn and Member widgets to generate the code.
+
+*)
+
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fabulous.AST.dll"
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.FCS.dll"
+
+open type Fabulous.AST.Ast
+
+Oak() {
+    AnonymousModule() {
+        TypeDefn(
+            "Person",
+            ParenPat(
+                TuplePat(
+                    [ ParameterPat(ConstantPat(Constant("name")))
+                      ParameterPat(ConstantPat(Constant("lastName")))
+                      ParameterPat(ConstantPat(Constant("age"))) ]
+                )
+            )
+        ) {
+            Member("this.Name", ConstantExpr("name"))
+            Member("this.LastName", ConstantExpr("lastName"))
+            Member("this.Age", ConstantExpr("age"))
+        }
+    }
+}
+|> Gen.mkOak
+|> Gen.run
+|> printfn "%s"
+// produces the following code:
+(*** include-output ***)
+
+(**
+#### Example 4: Generate unit of measure types
+- We will generate a unit of measure type with 6 cases `cm`, `ml`, `m`, `s`, `kg` and `N`.
+- We will use AnonymousModule, Union and UnionCase widgets to generate the code.
+*)
+
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fabulous.AST.dll"
+#r "../src/Fabulous.AST/bin/Release/netstandard2.1/publish/Fantomas.FCS.dll"
+
+open type Fabulous.AST.Ast
+
+Oak() {
+    AnonymousModule() {
+        Measure("cm").xmlDocs([ "Cm, centimeters." ])
+
+        Measure("ml", MeasurePower(LongIdent "cm", Integer "3"))
+            .xmlDocs([ "Ml, milliliters." ])
+
+        Measure("m").xmlDocs([ "Distance, meters." ])
+
+        Measure("s").xmlDocs([ "Time, seconds." ])
+
+        Measure("kg").xmlDocs([ "Mass, kilograms." ])
+
+        Measure(
+            "N",
+            Tuple(
+                [ AppPostfix(LongIdent "kg", LongIdent "m")
+                  MeasurePower(LongIdent "s", Integer "2") ],
+                "/"
+            )
+        )
+            .xmlDocs([ "Force, Newtons." ])
+    }
+}
 |> Gen.mkOak
 |> Gen.run
 |> printfn "%s"
@@ -107,9 +400,14 @@ Oak() { AnonymousModule() { EscapeHatch(topLevelBinding) } }
 (*** include-output ***)
 
 (**
-## Key takeaway
 
-Using Fabulous.AST, you can easily create and manipulate ASTs like this one using F# functions. For example, you can add new nodes to the AST, modify existing nodes, or traverse the AST to perform analysis or transformation tasks.
+#### Conclusion
+Hope by now you have a good understanding of what is an AST and how you can use it to generate code using Fabulous.AST.
 
-Fabulous.AST is a powerful tool for anyone who works with code and wants to automate or streamline their development workflow. Whether you're a compiler writer, a code generator, or just someone who wants to write better code faster, Fabulous.AST can help you achieve your goals.
+You can generate pretty much any F# code you want as long as you know how to represent it using the DSL provided by Fabulous.AST.
+
+#### References
+- [F# AST](https://fsharp.github.io/fsharp-compiler-docs/fcs/untypedtree-apis.html)
+- [Fantomas](https://fsprojects.github.io/fantomas/docs/end-users/GeneratingCode.html)
+- [Fantomas tools](https://fsprojects.github.io/fantomas-tools/)
 *)
