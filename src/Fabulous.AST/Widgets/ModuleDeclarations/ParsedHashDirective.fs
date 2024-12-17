c4a3f87904dcf50e697b0f02f6ab23a3bc199c38
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-12-17 14:08:08 +0000

    ParsedHashDirectives

--- src/Fabulous.AST/Widgets/ModuleDeclarations/ParsedHashDirective.fs
+++ src/Fabulous.AST/Widgets/ModuleDeclarations/ParsedHashDirective.fs
@@ -40,31 +40,134 @@ module HashDirectiveBuilders =
                 ParsedHashDirectives.Arguments.WithValue(arguments)
             )
 
+        /// <summary>Creates a NoWarn hash directive.</summary>
+        /// <param name="args">The arguments of the NoWarn hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         NoWarn([ String "0044"; String "0045" ])
+        ///     }
+        /// }
+        /// </code>
         static member NoWarn(args: WidgetBuilder<Constant> list) = Ast.BaseHashDirective("nowarn", args)
 
+        /// <summary>Creates a NoWarn hash directive.</summary>
+        /// <param name="args">The arguments of the NoWarn hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         NoWarn([ "0044"; "0045" ])
+        ///     }
+        /// }
+        /// </code>
         static member NoWarn(args: string list) =
             Ast.NoWarn(args |> List.map(Ast.Constant))
 
+        /// <summary>Creates a NoWarn hash directive.</summary>
+        /// <param name="value">The argument of the NoWarn hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         NoWarn(String "0044")
+        ///     }
+        /// }
+        /// </code>
         static member NoWarn(value: WidgetBuilder<Constant>) = Ast.NoWarn([ value ])
 
+        /// <summary>Creates a NoWarn hash directive.</summary>
+        /// <param name="value">The argument of the NoWarn hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         NoWarn("0044")
+        ///     }
+        /// }
+        /// </code>
         static member NoWarn(value: string) = Ast.NoWarn(Ast.Constant(value))
 
+        /// <summary>Creates a Help hash directive.</summary>
+        /// <param name="value">The arguments of the Help hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         Help(String("List.map"))
+        ///     }
+        /// }
+        /// </code>
         static member Help(value: WidgetBuilder<Constant>) =
             Ast.BaseHashDirective("help", [ value ])
 
+        /// <summary>Creates a Help hash directive.</summary>
+        /// <param name="value">The arguments of the Help hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         Help("List.map")
+        ///     }
+        /// }
+        /// </code>
         static member Help(value: string) = Ast.Help(Ast.Constant(value))
 
+        /// <summary>Creates a HashDirective widget</summary>
+        /// <param name="ident">The identifier of the hash directive.</param>
+        /// <param name="args">The arguments of the hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         HashDirective("help", [ String "List.map" ])
+        ///     }
+        /// }
+        /// </code>
         static member HashDirective(ident: string, args: WidgetBuilder<Constant> list) =
             Ast.BaseHashDirective(ident, args)
 
+        /// <summary>Creates a HashDirective widget</summary>
+        /// <param name="ident">The identifier of the hash directive.</param>
+        /// <param name="args">The arguments of the hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         HashDirective("help", [ "List.map" ])
+        ///     }
+        /// }
+        /// </code>
         static member HashDirective(ident: string, args: string list) =
             Ast.HashDirective(ident, args |> List.map(Ast.Constant))
 
+        /// <summary>Creates a HashDirective widget</summary>
+        /// <param name="ident">The identifier of the hash directive.</param>
+        /// <param name="value">The argument of the hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         HashDirective("help", String "List.map")
+        ///     }
+        /// }
+        /// </code>
         static member HashDirective(ident: string, value: WidgetBuilder<Constant>) = Ast.HashDirective(ident, [ value ])
 
+        /// <summary>Creates a HashDirective widget</summary>
+        /// <param name="ident">The identifier of the hash directive.</param>
+        /// <param name="value">The argument of the hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         HashDirective("help", "List.map")
+        ///     }
+        /// }
+        /// </code>
         static member HashDirective(ident: string, value: string) =
             Ast.HashDirective(ident, Ast.Constant(value))
 
+        /// <summary>Creates a HashDirective widget</summary>
+        /// <param name="ident">The identifier of the hash directive.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         HashDirective("help")
+        ///     }
+        /// }
+        /// </code>
         static member HashDirective(ident: string) = Ast.HashDirective(ident, [])
 
 type HashDirectiveNodeExtensions =
