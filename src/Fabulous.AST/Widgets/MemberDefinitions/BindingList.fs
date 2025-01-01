5a748ec5bfac53a6669fe6891f1746e4296984de
Edgar Gonzalez <egonzalez@totallymoney.com>
2025-01-01 13:29:03 +0000

    LetBindings

--- src/Fabulous.AST/Widgets/MemberDefinitions/BindingList.fs
+++ src/Fabulous.AST/Widgets/MemberDefinitions/BindingList.fs
@@ -16,12 +16,41 @@ module BindingList =
 module LetBindingMemberBuilders =
     type Ast with
 
+        /// <summary>
+        /// Define a list of let bindings.
+        /// </summary>
+        /// <param name="bindings">The list of bindings.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         TypeDefn("Person", UnitPat()) {
+        ///             LetBindings([
+        ///                 Value(ConstantPat(Constant "_name"), ConstantExpr(String(""))).toMutable()
+        ///                 Value(ConstantPat(Constant "_age"), ConstantExpr(String("0"))).toMutable()
+        ///             ])
+        ///         }
+        ///     }
+        /// }
+        /// </code>
         static member LetBindings(bindings: WidgetBuilder<BindingNode> list) =
             WidgetBuilder<BindingListNode>(
                 BindingList.WidgetKey,
                 BindingList.Bindings.WithValue(bindings |> List.map Gen.mkOak)
             )
 
+        /// <summary>
+        /// Define a single let binding.
+        /// </summary>
+        /// <param name="binding">The binding.</param>
+        /// <code language="fsharp">
+        /// Oak() {
+        ///     AnonymousModule() {
+        ///         TypeDefn("Person", UnitPat()) {
+        ///             LetBinding(Value(ConstantPat(Constant "_name"), ConstantExpr(String(""))).toMutable())
+        ///         }
+        ///     }
+        /// }
+        /// </code>
         static member LetBinding(binding: WidgetBuilder<BindingNode>) =
             WidgetBuilder<BindingListNode>(
                 BindingList.WidgetKey,
