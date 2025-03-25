5dd2c3827b21420cd6e267b8ac21d6a15cc122c1
Edgar Gonzalez <edgargonzalez.info@gmail.com>
2025-03-25 21:32:51 +0000

    Add a playground sample

---
+++ samples/Playground/Program.fs
@@ -0,0 +1,9 @@
+﻿namespace Playground
+
+module Main =
+
+    [<EntryPoint>]
+    let main _ =
+        Generator.source $"{__SOURCE_DIRECTORY__}/output.fs" |> Async.RunSynchronously
+
+        0 // return an integer exit code
